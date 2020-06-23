d24b4ce56ca8a497b2c8da3f35619acbe1aa384c
sroopsai <roopsai.work@gmail.com>
2020-06-23 23:18:12 +0200

    Added exercise2

---
+++ src/chapter1/section1/Exercise2.java
@@ -0,0 +1,16 @@
+package chapter1.section1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        double answerA = (1 + 2.236)/2;
+        double answerB = 1 + 2 + 3 + 4.0;
+        boolean answerC = 4.1 >= 4;
+        String answerD = 1 + 2 + "3";
+        StdOut.println("a) " + answerA + " --> double");
+        StdOut.println("b) " + answerB + " --> double");
+        StdOut.println("c) " + answerC + " --> boolean");
+        StdOut.println("d) " + answerD + " --> String");
+    }
+}
